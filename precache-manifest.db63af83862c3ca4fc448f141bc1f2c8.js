8ec0ca0693813bed19e109570259a491d39471cf
Serhii <vasylyk29@gmail.com>
2020-03-30 12:20:34 +0300

    Updates

---
+++ precache-manifest.db63af83862c3ca4fc448f141bc1f2c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0b652570098d3f5e3ab735f3219c49",
+    "url": "/cursor_react_foundations/index.html"
+  },
+  {
+    "revision": "47c34d7dd5fa95f21ffe",
+    "url": "/cursor_react_foundations/static/css/main.23a83e29.chunk.css"
+  },
+  {
+    "revision": "28e086e740e0a9fa1978",
+    "url": "/cursor_react_foundations/static/js/2.e27ebb78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor_react_foundations/static/js/2.e27ebb78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c34d7dd5fa95f21ffe",
+    "url": "/cursor_react_foundations/static/js/main.44c0700c.chunk.js"
+  },
+  {
+    "revision": "e71f36b03e68bd9063dd",
+    "url": "/cursor_react_foundations/static/js/runtime-main.d2ccdc8b.js"
+  }
+]);
